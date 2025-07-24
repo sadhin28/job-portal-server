ba26dcfbe9fd58c012a603da3454668d4538d5fa
sadhin28 <taosifsadhin113@gmail.com>
2025-07-24 21:38:57 +0600

    add apply data

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json({ limit: '50mb' }));
 app.get('/', (req, res) => {
     res.send('Job Portal Is Running')
 })
-app.get('/ping',(req,res)=>{
+app.get('/ping', (req, res) => {
     res.send('Pong')
 })
 
@@ -21,43 +21,60 @@ const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@clu
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    //create database
-    const JobPostsCollection=client.db('Job-Posted-Data').collection('PostedJobs')
-    const JobApplicationCollection=client.db('Job-Application-Data').collection('Job-Application')
-    const initialdatacollection=client.db('jobportal').collection('jobs')
-    
-    //get initial data
-    app.get('/jobs',async(req,res)=>{
-        const cursor = initialdatacollection.find();
-        const result = await cursor.toArray();
-        res.send(result)
-    })
-    
-   //get initial jobs data by id
-   app.get('/jobs/:id',async(req,res)=>{
-     const id =req.params.id;
-     const query={_id: new ObjectId(id)}
-     const result =await initialdatacollection.findOne(query)
-     res.send(result)
-   })
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-   
-  }
+    try {
+        //create database
+        const JobPostsCollection = client.db('Job-Posted-Data').collection('PostedJobs')
+        const JobApplicationCollection = client.db('Job-Application-Data').collection('Job-Application')
+        const initialdatacollection = client.db('jobportal').collection('jobs')
+        
+
+        //get initial data
+        app.get('/jobs', async (req, res) => {
+            const cursor = initialdatacollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        //get initial jobs data by id
+        app.get('/jobs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await initialdatacollection.findOne(query)
+            res.send(result)
+        })
+
+        //Post application
+        app.post('/apply',async(req,res)=>{
+            const newApply = req.body;
+            res.send(newApply);
+            const result = await JobApplicationCollection.insertOne(newApply);
+            res.send(result)
+        })
+
+        //get all application data
+        app.get('/apply',async(req,res)=>{
+            const cursor = JobApplicationCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        }) 
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
 }
 run().catch(console.dir);
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`job-portal-server is running:${port}`)
 })
