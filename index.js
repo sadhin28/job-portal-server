bb3ce16b8d5aaa3d12e998c8a01aa22dc769ea1b
sadhin28 <taosifsadhin113@gmail.com>
2025-07-26 19:44:59 +0600

    all functionality implement done

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ async function run() {
         const addPostsCollection = client.db('Job-Posted-Data').collection('PostedJobs')
         const JobApplicationCollection = client.db('Job-Application-Data').collection('Job-Application')
         const initialdatacollection = client.db('jobportal').collection('jobs')
-        
+
 
         //get initial data
         app.get('/jobs', async (req, res) => {
@@ -42,7 +42,7 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result)
         })
-        
+
         //get initial jobs data by id
         app.get('/jobs/:id', async (req, res) => {
             const id = req.params.id;
@@ -50,93 +50,122 @@ async function run() {
             const result = await initialdatacollection.findOne(query)
             res.send(result)
         })
-         //post a job
-         app.post('/jobs',async(req,res)=>{
+        //post a job
+        app.post('/jobs', async (req, res) => {
             const newJobPost = req.body;
             res.send(newJobPost);
             const result = await initialdatacollection.insertOne(newJobPost)
             res.send(result);
-         })
-         //delete posted job
-         app.delete('/jobs/:id',async(req,res)=>{
+        })
+        //delete posted job
+        app.delete('/jobs/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id:new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await initialdatacollection.deleteOne(query)
             res.send(result)
-         })
+        })
+        //update job
+        app.put("/jobs/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const options = { Upsert: true };
+            const updateJobs = req.body;
 
-       //get some data by email using query
-        app.get('/my-jobposts',async(req,res)=>{
-         const email = req.query.email;
-         const query = {hr_email : email}
-         const result = await initialdatacollection.find(query).toArray();
-         res.send(result)
+            let newUpdateJobs = {
+                $set: {
+                    title: updateJobs.title,
+                    location: updateJobs.location,
+                    jobType: updateJobs.jobType,
+                    category: updateJobs.category,
+                    applicationDeadline: updateJobs.applicationDeadline,
+                    salaryRange: updateJobs.salaryRange,
+                    description: updateJobs.description,
+                    company: updateJobs.company,
+                    requirements: updateJobs.requirements,
+                    responsibilities: updateJobs.responsibilities,
+                    status: updateJobs.status,
+                    hr_email: updateJobs.hr_email,
+                    hr_name: updateJobs.hr_name,
+                    company_logo: updateJobs.company_logo,
+                }
+            }
+            const result = await initialdatacollection.updateOne(filter, newUpdateJobs, options)
+            res.send(result)
+            console.log(updateJobs)
+        })
 
-       })
-       //
+        //get some data by email using query
+        app.get('/my-jobposts', async (req, res) => {
+            const email = req.query.email;
+            const query = { hr_email: email }
+            const result = await initialdatacollection.find(query).toArray();
+            res.send(result)
+
+        })
+        //
         //Post application
-        app.post('/apply',async(req,res)=>{
+        app.post('/apply', async (req, res) => {
             const newApply = req.body;
             res.send(newApply);
             const result = await JobApplicationCollection.insertOne(newApply);
-            
-           const id=newApply.job_id;
-           const query = {_id:new ObjectId(id)}
-           const job=await initialdatacollection.findOne(query);
-           let newcount =0
-           if(job.applicationCount){
-               newcount = job.applicationCount +1;
-           }else{
-             newcount=1;
-           }
-           //Now update newcount
-           const filter = {_id : new ObjectId(id)}
-           const updatedDoc ={
-            $set:{
-                applicationCount:newcount
+
+            const id = newApply.job_id;
+            const query = { _id: new ObjectId(id) }
+            const job = await initialdatacollection.findOne(query);
+            let newcount = 0
+            if (job.applicationCount) {
+                newcount = job.applicationCount + 1;
+            } else {
+                newcount = 1;
             }
-           }
-           const updateResult = await initialdatacollection.updateOne(filter,updatedDoc)
+            //Now update newcount
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    applicationCount: newcount
+                }
+            }
+            const updateResult = await initialdatacollection.updateOne(filter, updatedDoc)
             res.send(result)
         })
-        
+
         //get all application data
-        app.get('/apply',async(req,res)=>{
+        app.get('/apply', async (req, res) => {
             const cursor = JobApplicationCollection.find();
             const result = await cursor.toArray();
             res.send(result)
         })
-         //get application by job_id
-        app.get('/job-application/:job_id',async(req,res)=>{
+        //get application by job_id
+        app.get('/job-application/:job_id', async (req, res) => {
             const Jobid = req.params.job_id;
-            const query ={job_id:Jobid}
+            const query = { job_id: Jobid }
             const result = await JobApplicationCollection.find(query).toArray()
             res.send(result)
         })
         //get one applicatin by id
-          app.get('/apply/:id', async (req, res) => {
+        app.get('/apply/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await JobApplicationCollection.findOne(query)
             res.send(result)
         })
-       
+
 
         //delete application
-        app.delete('/apply/:id',async(req,res)=>{
-            const id=req.params.id;
-            const query = {_id:new ObjectId(id)};
+        app.delete('/apply/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
             const result = await JobApplicationCollection.deleteOne(query)
             res.send(result)
         })
-       //get some data by email
-       app.get('/job-application',async(req,res)=>{
-         const email = req.query.email;
-         const query = {applicant_email : email}
-         const result = await JobApplicationCollection.find(query).toArray();
-         res.send(result)
+        //get some data by email
+        app.get('/job-application', async (req, res) => {
+            const email = req.query.email;
+            const query = { applicant_email: email }
+            const result = await JobApplicationCollection.find(query).toArray();
+            res.send(result)
 
-       })
+        })
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
