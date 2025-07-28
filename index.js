409169ad10767c58c65f06aa692478aacf7082c4
sadhin28 <taosifsadhin113@gmail.com>
2025-07-28 21:51:46 +0600

    add secure

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ async function run() {
            
            res.cookie('token', token, {
                     httpOnly: true,
-                    secure: false,
+                    secure: true,
 
                 })
            .send({ success: true })
